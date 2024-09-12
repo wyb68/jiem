52673b8ccec038a86415879cdd00bdd24a8fa44c
AC王 <1067609278@qq.com>
2024-09-12 12:48:29 +0800

    update

--- run.py
+++ run.py
@@ -5,4 +5,5 @@ from wxcloudrun import app
 
 # 启动Flask Web服务
 if __name__ == '__main__':
-    app.run(host=sys.argv[1], port=sys.argv[2])
+    app.run(host=sys.argv[1], port=sys.argv[2])  # 上线
+    # app.run(host='0.0.0.0', port=5000, debug=True)  # 本地测试-启动程序
--- wxcloudrun/views.py
+++ wxcloudrun/views.py
@@ -4,6 +4,8 @@ from run import app
 from wxcloudrun.dao import delete_counterbyid, query_counterbyid, insert_counter, update_counterbyid
 from wxcloudrun.model import Counters
 from wxcloudrun.response import make_succ_empty_response, make_succ_response, make_err_response
+import os
+import re
 
 
 @app.route('/')
@@ -14,6 +16,99 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/api/model1', methods=['POST'])
+def model1():
+    params = request.get_json()
+    name = params.get("name")
+    time = params.get("time")
+    work = re.sub(r'[ \n]', '', params.get("work"))  # 正则去除空格和回车
+
+    # 设定文件夹和文件的路径
+    folder_path = './userData/'
+    file_path = os.path.join(folder_path, name + '.txt')
+    print(file_path)
+    # 检查userData文件夹是否存在，如果不存在则创建它
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+
+        # 检查文件是否存在，如果不存在则创建它（这里其实不需要显式创建文件，因为使用'a'模式打开文件时如果文件不存在会自动创建）
+    if not os.path.exists(file_path):
+        # 这里其实不需要执行具体的创建文件操作，因为下面使用'a'模式打开文件时会自动创建
+        print(f"文件'{file_path}'不存在，将在写入时自动创建。")
+
+        # 使用追加模式('a')打开文件，写入内容
+    with open(file_path, 'a', encoding='utf-8') as file:
+        file.write(time + '@@' + work + '\n')
+
+    return {
+        'success': True,
+        'message': '提交成功！'
+    }
+
+
+@app.route('/api/model2', methods=['POST'])
+def model2():
+    params = request.get_json()
+    name = params.get("name")
+    data = []
+    path = './userData/'
+    for filename in os.listdir(path):
+        if name == os.path.splitext(filename)[0] or name == '':
+            if os.path.isfile(os.path.join(path, filename)):
+                with open(path + filename, encoding='utf-8') as file:
+                    data0 = []
+                    for line in file:
+                        if line.strip():
+                            line_data = line.strip().replace('\n', '').split('@@')
+                            data0.append({
+                                "time": line_data[0],
+                                "content": line_data[1]
+                            })
+            data.append({
+                'user_name': os.path.splitext(filename)[0],
+                'user_data': data0
+            })
+    if len(data):
+        response_data = {
+            'success': True,
+            'message': '提交成功！',
+            'data': data
+        }
+    else:
+        response_data = {
+            'success': False,
+            'message': '未查到用户数据！',
+            'data': []
+        }
+    return response_data
+
+
+@app.route('/api/model3', methods=['POST'])
+def model3():
+    params = request.get_json()
+    name = params.get("name")
+    path = './userData/'
+    # 定义文件路径
+    file_path = os.path.join(path, name + '.txt')
+
+    # 检查文件是否存在
+    if os.path.exists(file_path):
+        # 删除文件
+        os.remove(file_path)
+        response_data = {
+            'success': True,
+            'message': '删除成功！'
+        }
+    else:
+        response_data = {
+            'success': False,
+            'message': '用户不存在！'
+        }
+
+    return response_data
+
+
+# 以下模板默认
 @app.route('/api/count', methods=['POST'])
 def count():
     """
